15024cd30a175475011f27acf73e56ccaa93810a
liha4ini <liha4ev.sergey@gmail.com>
2022-09-16 11:54:02 +0300

    task_31

---
+++ Falsy_Bouncer.js
@@ -0,0 +1,16 @@
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Hint: Try converting each value to a Boolean.
+
+
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    return newArray;
+}
+  
+bouncer([7, "ate", "", false, 9]);
