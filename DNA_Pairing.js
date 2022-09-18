4fab798fc1936f078e13d20e98b1dc510bf1c532
liha4ini <liha4ev.sergey@gmail.com>
2022-09-18 09:11:39 +0300

    task_51

---
+++ DNA_Pairing.js
@@ -0,0 +1,24 @@
+// Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+// The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+// For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+// The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+
+function pairElement(str) {
+    //create object for pair lookup
+    var pairs = {
+      A: "T",
+      T: "A",
+      C: "G",
+      G: "C"
+    };
+    //split string into array of characters
+    var arr = str.split("");
+    //map character to array of character and matching pair
+    return arr.map(x => [x, pairs[x]]);
+}
+  
+pairElement("GCG");
