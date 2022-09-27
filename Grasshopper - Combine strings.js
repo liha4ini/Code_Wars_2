ef4a2f457c0507338060830cf842f1ca5504dfd3
liha4ini <liha4ev.sergey@gmail.com>
2022-09-27 09:06:33 +0300

    task_109

---
+++ Grasshopper - Combine strings.js
@@ -0,0 +1,12 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+
+const combineNames = (fN, lN) => `${fN} ${lN}`;
