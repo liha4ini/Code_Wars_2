0b687c9b3900300b1685f74d1419a1eb17b69556
liha4ini <liha4ev.sergey@gmail.com>
2022-09-29 10:06:17 +0300

    task_121

---
+++ Is it a number_.js
@@ -0,0 +1,4 @@
+// Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+
+const isDigit = s => s == parseFloat(s);
