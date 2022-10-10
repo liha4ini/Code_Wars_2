61ddc3465c556988ce469635e550b65d17d77e96
liha4ini <liha4ev.sergey@gmail.com>
2022-10-10 08:30:40 +0300

    task_181

---
+++ Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,13 @@
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+
+const remove = s => s.replace(/!+$/, '');
