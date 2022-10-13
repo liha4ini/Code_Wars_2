51badccd9c5e14c0a4e1f7b5af3d90515890e899
liha4ini <liha4ev.sergey@gmail.com>
2022-10-13 09:48:00 +0300

    task_199

---
+++ Max Headroom and JavaScript style.js
@@ -0,0 +1,4 @@
+// Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+
+const getMax1 = getMax2 = () => ({name: 'Max Headroom '});
